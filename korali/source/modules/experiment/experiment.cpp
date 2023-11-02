5a80a5b5c8c76756bb27218f88858a837ec070ab
Sergey Litvinov <slitvinov@gmail.com>
2023-11-02 08:30:22 -0400

    rm unused modules

--- korali/source/modules/experiment/experiment.cpp
+++ korali/source/modules/experiment/experiment.cpp
@@ -5,8 +5,6 @@
 #include "modules/conduit/distributed/distributed.hpp"
 #include "modules/experiment/experiment.hpp"
 #include "modules/problem/problem.hpp"
-#include "modules/solver/agent/agent.hpp"
-#include "modules/solver/deepSupervisor/deepSupervisor.hpp"
 #include "modules/solver/solver.hpp"
 #include "sample/sample.hpp"
 #include <chrono>
