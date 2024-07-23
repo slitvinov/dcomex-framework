3ef7858dc0cb77933726243379ce4bf37b9ade17
Sergey Litvinov <slitvinov@gmail.com>
2024-07-23 10:43:11 +0200

    exec dotnet

--- poc/run.sh
+++ poc/run.sh
@@ -5,4 +5,4 @@ do if test $i -eq $n; then break; fi
    d=`printf %08d $i`
    echo $d
    i=$((i+1))
-done | xargs -P 64 -I{} -n 1 sh -c 'cd "{}" && dotnet ~/.local/share/MGroup.MSolve4Korali.dll MSolveInput.xml 0 2>stderr 1>stdout'
+done | xargs -P 64 -I{} -n 1 sh -c 'cd "{}" && exec dotnet ~/.local/share/MGroup.MSolve4Korali.dll MSolveInput.xml 0 2>stderr 1>stdout'
