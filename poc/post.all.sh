ce24a6916ff881143148a1eb698d10ed92e0b533
Sergey Litvinov <slitvinov@gmail.com>
2024-07-24 09:08:20 -0400

    add post.all.sh

---
+++ poc/post.all.sh
@@ -0,0 +1,12 @@
+: ${zip=B.zip}
+rm $zip
+
+m=0
+for d in [0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9]/
+do if test -f ${d}status
+   then r=`cat ${d}status`
+	m=$((m+1))
+	zip -qr9 "$zip" ${d} || exit 1
+   fi
+done
+echo $m "$zip"
