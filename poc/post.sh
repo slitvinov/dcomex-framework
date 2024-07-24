78433cf66be4db60c3a024948c1be62a68456503
Sergey Litvinov <slitvinov@gmail.com>
2024-07-24 08:38:08 -0400

    poc/post.sh

--- poc/post.sh
+++ poc/post.sh
@@ -1,13 +1,17 @@
+: ${zip=b.zip}
+rm $zip
+
+m=0 n=
 for d in [0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9]/
-do t=${d}tumorVolume_AnalysisNo_0.txt
-   e=${d}stderr
-   if test -f $t
-   then
-       n=`awk '{ for (i = 1; i <= NF; i++) if ($i == 0) {print i; exit} }' $t`
-       if test -s $e
-       then echo $d `test -s $e && echo 1`
-	    zip -qr9 f.zip ${d}MSolveInput.xml || exit 1
-	    zip -qr9 f.zip ${d}stderr || exit 1
-       fi
+do if test -f ${d}status
+   then r=`cat ${d}status`
+	m=$((m+1))
+	case $r in
+	    0) ;;
+	    *) n=$((n+1))
+	       zip -qr9 "$zip" ${d}MSolveInput.xml
+	       zip -qr9 "$zip" ${d}stderr ;;
+	esac
    fi
 done
+echo $n/$m "$zip"
