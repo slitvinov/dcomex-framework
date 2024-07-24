c87250942769325c522f2fd7441f40c4e29bc1d2
Sergey Litvinov <slitvinov@gmail.com>
2024-07-24 15:06:56 +0200

    add start, end

--- poc/run.sh
+++ poc/run.sh
@@ -1,3 +1,4 @@
+: ${P=64}
 n=1024
 i=0
 while :
@@ -5,4 +6,9 @@ do if test $i -eq $n; then break; fi
    d=`printf %08d $i`
    echo $d
    i=$((i+1))
-done | xargs -P 64 -I{} -n 1 sh -c 'cd "{}" && dotnet ~/.local/share/MGroup.MSolve4Korali.dll MSolveInput.xml 0 2>stderr 1>stdout; echo $? > status'
+done | xargs -P "$P" -I{} -n 1 sh -c \
+	     'cd "{}" &&
+	      date > start &&
+	      dotnet ~/.local/share/MGroup.MSolve4Korali.dll MSolveInput.xml 0 2>stderr 1>stdout
+	      echo $? > status
+	      data > end'
