4a5a77e8d2724f786c57c78616dbc57ae4c0651c
Sergey Litvinov <slitvinov@gmail.com>
2024-07-29 11:00:25 -0400

    add a.tar.gz

--- poc/post.tumor.sh
+++ poc/post.tumor.sh
@@ -5,7 +5,7 @@ m=0
 for d in [0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9]/
 do if test -f ${d}status
    then m=$((m+1))
-	 tar -rf "$tar" ${d}status ${d}stderr ${d}tumorVolume_AnalysisNo_0.txt || exit 1
+	 tar -rf "$tar" ${d}status ${d}stderr ${d}tumorVolume_AnalysisNo_0.txt ${d}MSolveInput.xml ${d}prescribedTimeSteppingList.txt || exit 1
    fi
 done
 gzip -f9 "$tar"
