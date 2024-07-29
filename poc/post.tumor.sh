7975dbe3d6489f33d7cf1572c65e7e7299c4e688
Sergey Litvinov <slitvinov@gmail.com>
2024-07-29 10:54:44 -0400

    add a.tar.gz

--- poc/post.tumor.sh
+++ poc/post.tumor.sh
@@ -1,11 +1,11 @@
-: ${zip=b.zip}
-rm $zip
+: ${tar=b.tar}
+rm $tar
 
 m=0
 for d in [0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9]/
 do if test -f ${d}status
    then m=$((m+1))
-	zip -qr9 "$zip" ${d}status ${d}stderr ${d}tumorVolume_AnalysisNo_0.txt || exit 1
+	 tar -rf "$tar" ${d}status ${d}stderr ${d}tumorVolume_AnalysisNo_0.txt || exit 1
    fi
 done
-echo $m "$zip"
+gzip -f9 "$tar"
