9973dd2c53da8afa21598b8aed37bd12fbca2749
Sergey Litvinov <slitvinov@gmail.com>
2024-08-06 08:29:23 -0400

    add timeStepTotalTimes_AnalysisNo_0.txt

--- poc/post.tumor.sh
+++ poc/post.tumor.sh
@@ -5,7 +5,14 @@ m=0
 for d in [0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9]/
 do if test -f ${d}status
    then m=$((m+1))
-	 tar -rf "$tar" ${d}status ${d}stderr ${d}tumorVolume_AnalysisNo_0.txt ${d}MSolveInput.xml ${d}prescribedTimeSteppingList.txt || exit 1
+	tar -rf "$tar" \
+	    ${d}MSolveInput.xml \
+	    ${d}prescribedTimeSteppingList.txt \
+	    ${d}status \
+	    ${d}stderr \
+	    ${d}timeStepTotalTimes_AnalysisNo_0.txt \
+	    ${d}tumorVolume_AnalysisNo_0.txt \
+	    || exit 1
    fi
 done
 gzip -f9 "$tar"
