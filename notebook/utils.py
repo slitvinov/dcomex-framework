6800ec725562ff335a5edc69804de1eac9b4ab8f
Sergey Litvinov <slitvinov@gmail.com>
2024-08-06 14:54:32 +0200

    update utils.py

--- notebook/utils.py
+++ notebook/utils.py
@@ -1,3 +1,4 @@
+import itertools
 import math
 import os
 import re
@@ -42,41 +43,24 @@ def read11(path):
             if re.match("^[0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9]/status$",
                         member.name):
                 dirname = re.sub("/status$", "", member.name)
-                content = tar.extractfile(
-                    dirname +
-                    "/prescribedTimeSteppingList.txt").read().decode('utf-8')
-                dt = []
-                nt = []
-                for line in content.split("\n"):
-                    if line:
-                        nt0, dt0 = line.split(",")
-                        nt.append(int(nt0))
-                        dt.append(float(dt0))
                 root = ET.parse(tar.extractfile(dirname + "/MSolveInput.xml"))
                 params = {
                     key.tag: float(key.text)
                     for key in root.find('./Parameters') if key.tag in KEYS
                 }
-                content = tar.extractfile(dirname +
-                                          "/status").read().decode('utf-8')
-                status = int(content)
-                content = tar.extractfile(
+                status = int(tar.extractfile(dirname +
+                                          "/status").read().decode('utf-8'))
+                volume_file = tar.extractfile(
                     dirname +
                     "/tumorVolume_AnalysisNo_0.txt").read().decode('utf-8')
-                volume = []
-                time = []
-                j = 0
-                t = 0
-                cnt = 0
-                for v in content.split():
+                time_file = tar.extractfile(
+                    dirname +
+                    "/timeStepTotalTimes_AnalysisNo_0.txt").read().decode('utf-8')
+                volume = [ ]
+                time = [ ]
+                for t, v in zip(time_file.split(), volume_file.split()):
                     if v == "0.000000E+000":
                         break
+                    time.append(float(t))
                     volume.append(float(v))
-                    t += dt[j]
-                    if cnt == nt[j] - 1 and j < len(nt) - 1:
-                        j += 1
-                        cnt = 0
-                    else:
-                        cnt += 1
-                    time.append(t)
                 yield params, time, volume, status
