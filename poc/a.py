2e95a198d8cfcfb8ee59745c6656ca931455a1d2
Sergey Litvinov <slitvinov@gmail.com>
2024-09-23 16:52:54 +0200

    add a.py

--- poc/a.py
+++ poc/a.py
@@ -5,7 +5,6 @@ mph = "RealisticMeshWithTetElements.mphtxt"
 csv = "t_ICs_realisticMesh_AdvancedModel.csv"
 data_dir = os.path.expanduser(os.path.join("~", ".local", "share"))
 
-imuno = True
 par = (
     ("miTumor", "rnd", 5, 50),  #
     ("k_th_tumor", "rnd", 7.5231e-12, 7.5231e-10),
@@ -13,13 +12,13 @@ par = (
     ("Sv", "rnd", 5000, 20000),
     ("k1", "rnd", 1.1574e-6, 3.9884e-06),
     ("Lp", "rnd", 1.56e-8, 7.47e-6),
-    ("sf", "rnd", 0.136, 0.146),
-    ("Per", "rnd", 4.16e-9, 6.225e-8),
-    ("K_T", "rnd", 1.2731E-6, 1.2731E-4),
-    ("k_on", "rnd", 1.1574E-8, 1.1574E-2),
-    ("kd", "rnd", 2.9981e+04, 9.2966e+04),
-    ("location", "rnd", 10, 14),
-    ("totalTimeNoImmuno", "rnd", 25, 25),
+    ("sf", "fix", 0.136),
+    ("Per", "fix", 4.16e-9),
+    ("K_T", "fix", 1.2731E-6),
+    ("k_on", "fix", 1.1574E-8),
+    ("kd", "fix", 2.9981e+04),
+    ("location", "fix", 10),
+    ("totalTimeNoImmuno", "fix", 25),
 )
 random.seed(12345)
 n = 1024
@@ -38,8 +37,9 @@ for i in range(n):
   <Output><TumorVolume/></Output>
   <Parameters>
 """ % (os.path.join(data_dir, mph), os.path.join(data_dir, csv)))
-        for name, type, a, b, *rest in par:
+        for name, type, a, *rest in par:
             if type == "rnd":
+                b, = rest
                 f.write("""\
     <%s>%.16e</%s>
 """ % (name, random.uniform(a, b), name))
@@ -49,10 +49,6 @@ for i in range(n):
 """ % (name, a, name))
             else:
                 assert False
-        if imuno:
-            f.write("""\
-    <includeImmuno/>
-""")
         f.write("""\
   </Parameters>
 </MSolve4Korali>
