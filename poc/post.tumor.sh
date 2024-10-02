9733786fb181cc502a4e9b3ace27cfc908992487
Sergey Litvinov <slitvinov@gmail.com>
2024-10-02 13:58:54 +0200

    then fi

--- poc/post.tumor.sh
+++ poc/post.tumor.sh
@@ -1,11 +1,13 @@
 : ${tar=~/1.tar}
 for d in [0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9]/
 do if test -f ${d}status
+   then
       tar -rf "$tar" \
 	  ${d}MSolveInput.xml \
 	  ${d}status \
 	  ${d}timeStepTotalTimes_AnalysisNo_1.txt \
 	  ${d}tumorVolume_AnalysisNo_1.txt \
 	  || exit 1
+   fi
 done
 # gzip -f9 "$tar"
