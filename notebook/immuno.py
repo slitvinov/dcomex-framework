9f92fd291b877ae8163820848316434724df4243
Sergey Litvinov <slitvinov@gmail.com>
2024-10-21 14:23:17 +0200

    add data_updated.xlsx

--- notebook/immuno.py
+++ notebook/immuno.py
@@ -3,16 +3,20 @@ import numpy as np
 import matplotlib.pyplot as plt
 import sys
 import collections
+import statistics
 
 
 def key(D):
     params, time, volume, status = D
-    return max(volume)
+    return max(volume) / volume[0]
 
 
+D = sorted(utils.read11("."), key=key)
+mvolume = (key(d) for d in D)
+*rest, volume90 = statistics.quantiles(mvolume, n=10)
 Status = collections.Counter()
 cnt = 0
-for params, time, volume, status in sorted(utils.read11("."), key=key):
+for params, time, volume, status in D:
     time = np.divide(time, 60 * 60 * 24)
     volume = np.divide(volume, volume[0])
     Status[status] += 1
@@ -30,10 +34,10 @@ for params, time, volume, status in sorted(utils.read11("."), key=key):
         plt.yscale("log")
         plt.xlabel("time (days)")
         plt.ylabel("relative tumor volume (log scale)")
-        plt.axis([None, None, 1, 100])
+        plt.axis([None, None, 1, volume90])
         plt.tight_layout()
         plt.savefig(path)
         plt.close()
-print(cnt)
+sys.stderr.write(f"simulatins     status\n")
 for k in sorted(Status):
-    print(k, Status[k])
+    sys.stderr.write(f"{Status[k]:10} {k:10}\n")
