17e1056dfef0a95e864ceae1ed6e2a4e2865041e
Sergey Litvinov <slitvinov@gmail.com>
2024-10-21 17:19:48 +0200

    updating 3.tar.gz

--- notebook/immuno.py
+++ notebook/immuno.py
@@ -4,6 +4,7 @@ import matplotlib.pyplot as plt
 import sys
 import collections
 import statistics
+import re
 
 
 def key(D):
@@ -11,22 +12,40 @@ def key(D):
     return max(volume) / volume[0]
 
 
+def experiment():
+    E = utils.experiment("data.xlsx")
+    for (typ, name), (time, volume) in E.items():
+        if re.match("^control", name):
+            time0 = utils.tstart[typ]
+            color = utils.color[typ]
+            try:
+                i = time.index(time0)
+                volume0 = volume[i]
+            except ValueError:
+                volume0, = np.interp([time0], time, volume)
+            plt.plot(np.subtract(time, time0),
+                     np.divide(volume, volume0),
+                     color + 'o-',
+                     alpha=0.5)
+
+
 D = sorted(utils.read11("."), key=key)
 mvolume = (key(d) for d in D)
-*rest, volume90 = statistics.quantiles(mvolume, n=10)
+*rest, volume_max = statistics.quantiles(mvolume, n=50)
 Status = collections.Counter()
 cnt = 0
 for params, time, volume, status in D:
     time = np.divide(time, 60 * 60 * 24)
     volume = np.divide(volume, volume[0])
     Status[status] += 1
-    color = "k" if status == 0 else "r"
-    plt.plot(time, volume, color + "-", alpha=0.5)
-    if params["includeImmuno"]:
-        location = params["location"]
-        index = np.searchsorted(time, location)
-        if index < len(volume):
-            plt.plot([location], [volume[index]], color + "o", alpha=0.5)
+    # color = "k" if status == 0 else "r"
+    if status == 0:
+        plt.plot(time, volume, "k", alpha=0.5)
+        if params["includeImmuno"]:
+            location = params["location"]
+            index = np.searchsorted(time, location)
+            if index < len(volume):
+                plt.plot([location], [volume[index]], "k")
     cnt += 1
     if cnt % 16 == 0:
         path = f"immuno.{cnt:05}.png"
@@ -34,7 +53,8 @@ for params, time, volume, status in D:
         plt.yscale("log")
         plt.xlabel("time (days)")
         plt.ylabel("relative tumor volume (log scale)")
-        plt.axis([None, None, 1, volume90])
+        experiment()
+        plt.axis([0, None, 1, volume_max])
         plt.tight_layout()
         plt.savefig(path)
         plt.close()
