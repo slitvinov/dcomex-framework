f071d8bab60bc1c9870456d66857eb2b85e2f21d
Sergey Litvinov <slitvinov@gmail.com>
2025-04-09 15:40:58 +0200

    sort parameters

--- set4/a.py
+++ set4/a.py
@@ -6,22 +6,22 @@ csv = "tICsgmsh1752.csv"
 data_dir = os.path.expanduser(os.path.join("~", ".local", "share"))
 
 par = (
-    ("location", "fix", 7.01),
+    ("includeImmuno", "fix", "false"),
+    ("k1", "rnd", 1.1574e-6, 3.9884e-06),
+    ("kd", "fix", 2.9981e+04),
+    ("k_on", "fix", 1.1574E-8),
+    ("K_T", "fix", 1.2731E-6),
+    ("k_th_tumor", "rnd", 7.5231e-12, 7.5231e-10),
     ("location_2", "fix", 9.01),
     ("location_3", "fix", 11.01),
-    ("includeImmuno", "fix", "false"),
-    ("useSingleImmunoTherapy", "fix", "false"),
-    ("sf", "fix", 0.136),
-    ("Per", "fix", 4.16e-9),
-    ("K_T", "fix", 1.2731E-6),
-    ("k_on", "fix", 1.1574E-8),
-    ("kd", "fix", 2.9981e+04),
-    ("miTumor", "rnd", 5, 50),
-    ("k_th_tumor", "rnd", 7.5231e-12, 7.5231e-10),
-    ("pv", "rnd", 0.6666, 6.6661),
-    ("Sv", "rnd", 5000, 20000),
-    ("k1", "rnd", 1.1574e-6, 3.9884e-06),
+    ("location", "fix", 7.01),
     ("Lp", "rnd", 1.56e-8, 5e-7),
+    ("miTumor", "rnd", 5, 50),
+    ("Per", "fix", 4.16e-9),
+    ("pv", "rnd", 0.6666, 6.6661),
+    ("sf", "fix", 0.136),
+    ("Sv", "rnd", 5000, 20000),
+    ("useSingleImmunoTherapy", "fix", "false"),
 )
 random.seed(12345)
 for i in range(1024):
