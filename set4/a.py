dbafd6dbafe943b1f2d4d91d305fbfd2c34d0592
Sergey Litvinov <slitvinov@gmail.com>
2025-04-09 15:50:04 +0200

    add production a.py Acked-by: Sergey Litvinov <slitvinov@gmail.com>

--- set4/a.py
+++ set4/a.py
@@ -7,21 +7,21 @@ data_dir = os.path.expanduser(os.path.join("~", ".local", "share"))
 
 par = (
     ("includeImmuno", "fix", "true"),
-    ("k1", "rnd", 1.1574e-6, 3.9884e-06),
-    ("kd", "rnd", 2.9981e+04, 9.2966e+04),
+    ("k1", "uniform", 1.1574e-6, 3.9884e-06),
+    ("kd", "uniform", 2.9981e+04, 9.2966e+04),
     ("keepLogs", "fix", "true"),
-    ("k_on", "rnd", 1.1574e-8, 1.1574e-2),
-    ("k_th_tumor", "rnd", 7.5231e-12, 7.5231e-10),
-    ("K_T", "rnd", 1.2731e-6, 1.2731e-4),
-    ("location_2", "randint", (6, 7, 8)),
-    ("location_3", "randint", (9, 10, 11)),
-    ("location", "randin", (3, 4, 5)),
-    ("Lp", "rnd", 1.56e-8, 5e-7),
-    ("miTumor", "rnd", 5, 50),
-    ("Per", "rnd", 4.16e-9, 6.225e-8),
-    ("pv", "rnd", 0.6666, 6.6661),
-    ("sf", "rnd", 0.136, 0.146),
-    ("Sv", "rnd", 5000, 20000),
+    ("k_on", "uniform", 1.1574e-8, 1.1574e-2),
+    ("k_th_tumor", "uniform", 7.5231e-12, 7.5231e-10),
+    ("K_T", "uniform", 1.2731e-6, 1.2731e-4),
+    ("location_2", "choice", (6, 7, 8)),
+    ("location_3", "choice", (9, 10, 11)),
+    ("location", "choice", (3, 4, 5)),
+    ("Lp", "uniform", 1.56e-8, 5e-7),
+    ("miTumor", "uniform", 5, 50),
+    ("Per", "uniform", 4.16e-9, 6.225e-8),
+    ("pv", "uniform", 0.6666, 6.6661),
+    ("sf", "uniform", 0.136, 0.146),
+    ("Sv", "uniform", 5000, 20000),
     ("useSingleImmunoTherapy", "fix", "false"),
 )
 random.seed(12345)
@@ -44,7 +44,7 @@ for i in range(1024):
   <Parameters>
 """ % (os.path.join(data_dir, mph), os.path.join(data_dir, csv)))
         for name, type, a, *rest in par:
-            if type == "rnd":
+            if type == "uniform":
                 b, = rest
                 f.write("""\
     <%s>%.16e</%s>
@@ -54,10 +54,10 @@ for i in range(1024):
                     f.write("""\
     <%s>%s</%s>
 """ % (name, a, name))
-                else:
-                    f.write("""\
-    <%s>%.16e</%s>
-""" % (name, a, name))
+            elif type == "choice":
+                f.write("""\
+    <%s>%s</%s>
+""" % (name, random.choice(a), name))
             else:
                 assert False
         f.write("""\
